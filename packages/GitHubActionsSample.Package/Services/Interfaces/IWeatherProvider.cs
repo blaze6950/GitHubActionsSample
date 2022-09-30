0724bd6b500c302248dcb303348f388663569c06
Mykyta Zotov <mykyta.zotov@ihsmarkit.com>
2022-09-30 13:30:17 +0300

    fix: missed files were added; gitignore was fixed

---
+++ packages/GitHubActionsSample.Package/Services/Interfaces/IWeatherProvider.cs
@@ -0,0 +1,8 @@
+﻿using GitHubActionsSample.Package.Models;
+
+namespace GitHubActionsSample.Package.Services.Interfaces;
+
+public interface IWeatherProvider
+{
+    Task<IEnumerable<WeatherForecast>> GetWeatherForecastAsync();
+}
---
+++ packages/GitHubActionsSample.Package/Services/MockWeatherProvider.cs
@@ -0,0 +1,19 @@
+﻿using GitHubActionsSample.Package.Models;
+using GitHubActionsSample.Package.Services.Interfaces;
+
+namespace GitHubActionsSample.Package.Services;
+
+internal class MockWeatherProvider : IWeatherProvider
+{
+    private static readonly string[] Summaries = {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    public Task<IEnumerable<WeatherForecast>> GetWeatherForecastAsync() =>
+        Task.FromResult(Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateTime.Now.AddDays(index),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        }));
+}
