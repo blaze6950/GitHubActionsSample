9dd988c831831e0070e96b86e1d64722db3ae675
Mykyta Zotov <mykyta.zotov@ihsmarkit.com>
2022-09-30 13:27:10 +0300

    refactor: some refactoring was done

--- GitHubActionsSample.Package/Services/Interfaces/IWeatherProvider.cs
+++
@@ -1,8 +0,0 @@
-﻿using GitHubActionsSample.Package.Models;
-
-namespace GitHubActionsSample.Package.Services.Interfaces;
-
-public interface IWeatherProvider
-{
-    Task<IEnumerable<WeatherForecast>> GetWeatherForecastAsync();
-}
